f808f42aa124ccce0d69183034ad8502cfa7401f
Hemant Holani <hemant.holani18@gmail.com>
2017-11-09 23:57:58 +0530

    Image now loading without Glide

--- marknish/src/main/java/com/tanishqsharma/marknish/ImageLoader.java
+++ marknish/src/main/java/com/tanishqsharma/marknish/ImageLoader.java
@@ -3,7 +3,6 @@ package com.tanishqsharma.marknish;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.util.Log;
 import android.widget.ImageView;
 
 import java.io.InputStream;
@@ -29,11 +28,10 @@ public class ImageLoader extends AsyncTask<String, Void, Bitmap> {
         String url = urls[0];
         Bitmap bitmap = null;
         try {
-            URL urlconnection = new URL(url);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) urlconnection.openConnection();
+            URL urlConnection = new URL(url);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection.openConnection();
             httpURLConnection.setDoInput(true);
             httpURLConnection.connect();
-            Log.d(TAG, "doInBackground: " + url);
             InputStream inputStream = httpURLConnection.getInputStream();
             bitmap = BitmapFactory.decodeStream(inputStream);
         } catch (Exception e) {
