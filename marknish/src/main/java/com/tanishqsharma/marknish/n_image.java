1a8c2141867419fce764a6a918857d8ebff1c4fa
Tanishq Sharma <tanishqsh@gmail.com>
2017-11-09 23:45:52 +0530

    removed glide and the toast

--- marknish/src/main/java/com/tanishqsharma/marknish/n_image.java
+++ marknish/src/main/java/com/tanishqsharma/marknish/n_image.java
@@ -13,8 +13,7 @@ public class n_image extends android.support.v7.widget.AppCompatImageView {
     public n_image(Context context, String url) {
         super(context);
         this.setLayoutParams(new LinearLayout.LayoutParams(1000, 1000));
-        Toast.makeText(context, ""+url, Toast.LENGTH_SHORT).show();
-        Glide.with(context).load(url).into(this);
+
 
     }
 }
